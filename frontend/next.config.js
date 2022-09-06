0111e5fd039bdb69f27311e2afead0b2b051a1f9
Yeom-JinHo <pwlsghq@naver.com>
2022-09-06 17:06:41 +0900

    setup : sass

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `
+            @import "_variables.scss";
+            @import "_utils.scss";
+          `
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/reset.css";
+import "../styles/font.css";
+import "../styles/_typography.scss";
+import "../styles/_common.scss";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
