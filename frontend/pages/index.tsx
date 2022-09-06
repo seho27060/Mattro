53c6a37dbd7dc9cbccd3bb804f25682c620ea5e1
Yeom-JinHo <pwlsghq@naver.com>
2022-09-06 17:36:51 +0900

    setup : eslint

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return <div className="fs-20 flex">hello</div>;
