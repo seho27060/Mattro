5aeb87001942d275456d3d9b2fddf5b8b2872351
염진호 <pwlsghq@naver.com>
2022-09-14 16:59:43 +0900

    feat : 지하철 노선 데스크탑 퍼블리싱

---
+++ frontend/constants/lineInfo.ts
@@ -0,0 +1,18 @@
+const lineInfos = [
+  { id: "L1", name: "1" },
+  { id: "L2", name: "2" },
+  { id: "L3", name: "3" },
+  { id: "L4", name: "4" },
+  { id: "L5", name: "5" },
+  { id: "L6", name: "6" },
+  { id: "L7", name: "7" },
+  { id: "L8", name: "8" },
+  { id: "L9", name: "9" },
+  { id: "LK", name: "경의중앙" },
+  { id: "LB", name: "수인분당" },
+  { id: "LS", name: "신분당" },
+  { id: "LW", name: "우아신설" },
+  { id: "LSL", name: "신림" }
+];
+
+export default lineInfos;
