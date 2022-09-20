8ff9c5268cc9e13ae234c4fa0ae6d5051a4d1790
조성민 <chob5864@gmail.com>
2022-09-20 10:30:27 +0900

    build: 기본 코드

---
+++ spark-master/src/test/java/com/spark/spark/SparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
