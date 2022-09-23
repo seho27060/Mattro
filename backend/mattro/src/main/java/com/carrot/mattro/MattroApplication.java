6231d91302ec764d7eeeb512834f8c598448bf31
조성민 <chob5864@gmail.com>
2022-09-23 17:38:35 +0900

    Dev

---
+++ backend/mattro/src/main/java/com/carrot/mattro/MattroApplication.java
@@ -0,0 +1,13 @@
+package com.carrot.mattro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattroApplication.class, args);
+	}
+
+}
---
+++ backend/mattro/src/main/java/com/carrot/mattro/UserPreferApiController.java
@@ -0,0 +1,11 @@
+package com.carrot.mattro;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class UserPreferApiController {
+
+}
