0d81e82e7f85e39bb6783be5d6ad745d02c79021
이정민 <jmlee9707@naver.com>
2022-09-23 09:53:28 +0900

    feat : 메인페이지 기차 움직임, 메뉴바 애니메이션

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,25 @@
 import type { NextPage } from "next";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import logo from "../public/images/logo/logo.png";
-
+import train from "../public/images/train.png";
 import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
   const router = useRouter();
-  // const startClick = () => {
-  //   router.push("/theme/main"); // 페이지 이동
-  // };
+  const [clicked, setClicked] = useState(false);
+  const startClick = () => {
+    setClicked(true);
+    setTimeout(function () {
+      router.push("/theme/main");
+    }, 3000);
+  };
+
   return (
     <div className={`${styles.home}`}>
       <div className={`${styles.contents} flex column align-center`}>
-        <div className={`${styles.subtitle} fs-50 coreExtra`}>
+        <div className={`${styles.subtitle} fs-40 coreExtra`}>
           지하철 노선별 맛집 추천
           <div />
         </div>
@@ -25,12 +31,15 @@ const Home: NextPage = () => {
         <button
           type="button"
           onClick={() => {
-            router.push("/theme/main");
+            startClick();
           }}
           className={`${styles.btn} fs-40 coreExtra`}
         >
           START
         </button>
+        <div className={clicked ? styles.train_out : styles.train}>
+          <Image src={train} alt="train" className="styles.img" />
+        </div>
       </div>
     </div>
   );
