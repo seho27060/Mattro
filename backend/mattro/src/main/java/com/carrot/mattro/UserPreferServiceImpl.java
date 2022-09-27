cc39bb6db23c7352c14f8e3307e6f568e7b0a2e5
seho <seho27060@gmail.com>
2022-09-27 17:56:51 +0900

    fix:사용자선택기반 ëªAPI 선택값 입력에 따른 결과 반환

--- backend/mattro/src/main/java/com/carrot/mattro/UserPreferServiceImpl.java
+++ backend/mattro/src/main/java/com/carrot/mattro/UserPreferServiceImpl.java
@@ -19,8 +19,34 @@ public class UserPreferServiceImpl implements UserPreferService {
     private final CrawlingRepository crawlingRepository;
     @Override
     public List<CrawlingResponse> userPrefer(String choices) {
-        List<Crawling> byFoodCategoryIn = crawlingRepository.findByFoodCategoryIn(koreanFoodList);
-        Optional<List<Crawling>> test = Optional.ofNullable(byFoodCategoryIn);
+        Boolean index0 = false;
+        Boolean index1 = false;
+        Boolean index2 = false;
+
+        List<String> koreanFoodList = List.of("한식");
+        List<String> meetFoodList = List.of("육류,고기요리","족발,보쌈");
+        List<String> resultFoodCategoryList = new ArrayList<String>();
+
+        if(choices.substring(0,1).equals("1")){
+            index0 = true;
+        }
+        if(choices.substring(1,2).equals("1")){
+            index1 = true;
+        }
+        if(choices.substring(2,3).equals("1")){
+            index2 = true;
+        }
+        if(choices.substring(3,4).equals("1")){
+            resultFoodCategoryList.addAll(koreanFoodList);
+        }
+        if(choices.substring(4,5).equals("1")){
+            resultFoodCategoryList.addAll(meetFoodList);
+        }
+        List<Crawling> byUserPrefer = crawlingRepository.findByUserPrefer(index0,index1,index2,resultFoodCategoryList);
+//        키워드기반 리스트
+        Optional<List<Crawling>> test = Optional.ofNullable(byUserPrefer);
+
+//        추천알고리즘 적용
 
         List<CrawlingResponse> outer = new ArrayList<CrawlingResponse>();
         if (test.isPresent()){
