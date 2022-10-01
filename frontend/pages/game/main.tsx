2fed5c43407d8ba3e112313644f6afb9f87af190
윤정원 <wjd10577@naver.com>
2022-10-01 19:56:30 +0900

    fix : url path 옵션 추가

--- frontend/pages/game/main.tsx
+++ frontend/pages/game/main.tsx
@@ -13,7 +13,7 @@ import styles from "./main.module.scss";
 const socket =
   process.env.NODE_ENV === "development"
     ? io("ws://localhost:8000")
-    : io("wss://j7c206.p.ssafy.io", { path: "/nodejs/socket.io" });
+    : io("wss://j7c206.p.ssafy.io", { path: "/node/socket.io" });
 
 const Main: NextPage = () => {
   const roomStartRef = useRef<{
