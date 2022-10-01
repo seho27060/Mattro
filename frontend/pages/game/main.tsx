0d48c7a60beebe6a9d04b160ef68c8a3f36016d5
윤정원 <wjd10577@naver.com>
2022-10-01 19:13:21 +0900

    ws://j7c206.p.ssafy.io/node로 변경

--- frontend/pages/game/main.tsx
+++ frontend/pages/game/main.tsx
@@ -13,7 +13,7 @@ import styles from "./main.module.scss";
 const socket =
   process.env.NODE_ENV === "development"
     ? io("ws://localhost:8000")
-    : io("wss://j7c206.p.ssafy.io/node");
+    : io("ws://j7c206.p.ssafy.io/node");
 
 const Main: NextPage = () => {
   const roomStartRef = useRef<{
