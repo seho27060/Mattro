577f80d1e53dfc31bc3edd44f7ff6853f156e9de
조성민 <chob5864@gmail.com>
2022-10-03 13:57:06 +0900

    fix : 크로스오리진 해결 및 인코딩,디코딩 utf-8로 설정

--- backend/mattro/src/main/java/com/carrot/mattro/controller/RecommendationController.java
+++ backend/mattro/src/main/java/com/carrot/mattro/controller/RecommendationController.java
@@ -4,11 +4,9 @@ import com.carrot.mattro.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/subway/recommendation/find")
 @RequiredArgsConstructor
