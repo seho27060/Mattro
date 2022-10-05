4084e5e03f2dee8baf2d36ac966c337aa8aae89b
jmlee9707 <jmlee9707@naver.com>
2022-10-05 11:50:44 +0900

    fix : 리뷰 반영

--- frontend/pages/theme/[...params].tsx
+++ frontend/pages/theme/[...params].tsx
@@ -8,19 +8,16 @@ import { indexRes, themeRecommend } from "../api/recommend";
 import { storeDataType } from "../../constants/storeData";
 import { searchByName } from "../../constants/lineData";
 import prevBtn from "../../public/icons/prev.svg";
-import nextBtn from "../../public/icons/prev.svg";
-import { rootCertificates } from "tls";
 
 const Result = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
   const [choices, storeIndex]: any = router.query.params || [];
   const [foodList, setFoodList] = useState([]);
-  const [storeList, setStoreList] = useState<string[]>([]);
+  // const [storeList, setStoreList] = useState<string[]>([]);
 
   // ==============================
   // carosel
-  const TOTAL_SLIDES = 4; // 전체 슬라이드의 개수 0-4번 인덱스 총 5개
   const [currentSlide, setCurrentSlide] = useState<number>(0); // 현재 보여주고 있는 슬라이드
   const slideRef = useRef<HTMLDivElement>(null);
   const nextSlide = () => {
@@ -38,6 +35,7 @@ const Result = () => {
     }
   };
 
+  // 슬라이드 바뀔때마다 애니메이션
   useEffect(() => {
     if (slideRef.current) {
       slideRef.current.style.transition = "all 0.5s ease-in-out";
@@ -53,13 +51,14 @@ const Result = () => {
   // 제일 처음 실행
   useEffect(() => {
     setIsLoading(true);
-
-    // 즉시 실행 함수
-    (async function () {
-      //  api 호출
-      const res = await indexRes(storeIndex);
-      setFoodList(res);
-    })();
+    if (storeIndex !== undefined) {
+      // 즉시 실행 함수
+      (async function () {
+        //  api 호출
+        const res = await indexRes(storeIndex);
+        setFoodList(res);
+      })();
+    }
 
     setTimeout(() => {
       setIsLoading(false);
@@ -68,28 +67,28 @@ const Result = () => {
     console.log(foodList);
   }, [storeIndex]);
 
+  // url 확인
   // useEffect(() => {
   //   // 정규 표현식
   //   const numTest = /^[0-1]{5}$/;
 
-  //   if (numTest.test(choices) === false) {
-  //     router.push("/404");
-  //   }
-  //   if (router.query.params?.length !== 2) {
-  //     router.push("/404");
+  //   if (choices !== undefined) {
+  //     if (numTest.test(choices) === false) {
+  //       router.push("/404");
+  //     }
+  //     if (router.query.params?.length !== 2) {
+  //       router.push("/404");
+  //     }
   //   }
   // }, []);
+
   const again = () => {
     // api 재호출
     setIsLoading(true);
-
     (async function () {
       const res = await themeRecommend(choices);
-      console.log(res);
-      setStoreList(res);
-      const index = res.join();
+      const index = res.join(",");
       router.push(`/theme/${choices}/${index}`);
-      // setIsFinished(true);
     })();
   };
 
--- frontend/pages/theme/question.tsx
+++ frontend/pages/theme/question.tsx
@@ -5,24 +5,20 @@ import styles from "./question.module.scss";
 import QuestionCard from "../../components/theme/QuestionCard";
 import Stepbar from "../../components/theme/Stepbar";
 import { themeRecommend } from "../api/recommend";
-import { setMaxListeners } from "events";
 
 const question = () => {
   const [start, setStart] = useState<number>(0);
   const router = useRouter();
   const [choices, setChoices] = useState<string>("");
   // const [storeList, setStoreList] = useState<CardPropsType[]>([]);
-  const [storeList, setStoreList] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [storeList, setStoreList] = useState<string[]>([]);
+  // const [isLoading, setIsLoading] = useState(true);
 
   const moveLoading = () => {
     // api 호출
     async function getList() {
       const res = await themeRecommend(choices);
-      // storeIdx 5개 담기
-      setStoreList(res); // indec 배열 담기
-      // list의 배열 5개를 join해서 보내기
-      const storeIndex = res.join();
+      const storeIndex = res.join(",");
       router.push(`/theme/${choices}/${storeIndex}`);
     }
     getList();
