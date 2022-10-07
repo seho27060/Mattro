b9e58be27385eceb758f9e45eee131202015ec41
조성민 <chob5864@gmail.com>
2022-10-07 10:34:39 +0900

    Backend

---
+++ backend/mattro/src/main/java/com/carrot/mattro/controller/RecommendationController.java
@@ -0,0 +1,34 @@
+package com.carrot.mattro.controller;
+
+import com.carrot.mattro.service.RecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/subway/recommendation/find")
+@RequiredArgsConstructor
+public class RecommendationController {
+
+    private final RecommendationService recommendationService;
+    private final String EMPTY_RESULT = "empty_result";
+    private final String EMPTY_SUBWAY = "empty_subway";
+
+//    http://localhost:8080/api/subway/recommendation/find/남성
+    @GetMapping("/{subwayName}")
+    public ResponseEntity<String> recommendStore(@PathVariable String subwayName) {
+        String recommend = recommendationService.recommendationStore(subwayName);
+
+        if(recommend.equals(EMPTY_SUBWAY)){
+            return new ResponseEntity("역이 없습니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        if(recommend.equals(EMPTY_RESULT)){
+            return new ResponseEntity("데이터가 없습니다.", HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity(recommend, HttpStatus.OK);
+    }
+}
---
+++ backend/mattro/src/test/java/com/carrot/mattro/Repository/OutputRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.carrot.mattro.Repository;
+
+import com.carrot.mattro.domain.entity.Output;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+
+@SpringBootTest
+class OutputRepositoryTest {
+
+
+    @Autowired
+    OutputRepository outputRepository;
+    @Test
+    void findBy역명() {
+        String subwayName = "논현";
+        List<Output> output = outputRepository.findAllBy역명(subwayName);
+        assertThat(output).isNotEmpty();
+        assertThat(output.get(0).get역명()).isEqualTo(subwayName);
+    }
+}
